637cdb1223a40a317f7ebff5923bde11367fd5a6
YoungbeenNoh <youngbeennoh@gmail.com>
2020-12-03 23:38:19 +0900

    배열 data for문과 파라미터로 생성

--- miniposts.js
+++ miniposts.js
@@ -1,9 +1,21 @@
-function render() {
+const data = [
+    {title : '안녕하세요' ,
+    content : '내용'    
+    },
+    {title : '안녕하세요2 ',
+    content : '내용2'
+    },
+    {title : '안녕하세요3',
+    content : '내용3'
+    }
+]
+
+function render(data) {
     const list = document.querySelector('#list')
     const div = document.createElement('div')
 
     list.append(div)
-    div.innerHTML += '<h2>안녕하세요</h2><a href="./update.html">수정</a><button id="deleteButton">삭제</button><p>내용을 입력부분</p><i class="far fa-heart"></i>';
+    div.innerHTML += `<h2>${data.title}</h2><a href="./update.html">수정</a><button id="deleteButton">삭제</button><p>${data.content}</p><i class="far fa-heart"></i>`;
 
     const deleteButton = div.querySelector('button')
     deleteButton.addEventListener('click', function(e){
@@ -15,12 +27,12 @@ function render() {
     likeButton.addEventListener('click', function(e){
         likeButtonClick(e.target)
     })
-        
 }
 
-render()
-render()
-render()
+for(let i = 0; i < data.length; i++){
+    render(data[i])
+}
+
 
 function deleteDiv(div){
     let removeThis = confirm('삭제하시겠습니까?')
